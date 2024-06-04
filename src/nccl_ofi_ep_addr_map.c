da51fdbea1bd860ccfb9a89d41019034a4b05c57
Eric Raut <eraut@amazon.com>
2024-06-04 16:22:39 -0700

    Added some comments to the ep_addr_map code
    Signed-off-by: Eric Raut <eraut@amazon.com>

--- src/nccl_ofi_ep_addr_map.c
+++ src/nccl_ofi_ep_addr_map.c
@@ -11,37 +11,42 @@
 #include "contrib/uthash.h"
 #include "contrib/utlist.h"
 
-#include <rdma/fi_domain.h>
-
+/**
+ * A Libfabric address, stored in a form hashable by uthash
+ */
 typedef struct {
 	char addr[MAX_EP_ADDR];
 	UT_hash_handle hh;
 } hashed_addr_t;
 
+/**
+ * A struct storing a pair of (ep, addr_set)
+ */
 typedef struct {
 	nccl_net_ofi_ep_t *ep;
+	/* Hash set of addresses the endpoint is connected to */
 	hashed_addr_t *addr_set;
 } pair_ep_addr_set_t;
 
+/**
+ * A linked list of pairs of (ep, HashSet<addr>). The list should be stored in
+ * the calling code as a pointer to this struct.
+ */
 struct ep_pair_list_elem {
 	pair_ep_addr_set_t pair;
 	struct ep_pair_list_elem *prev;
 	struct ep_pair_list_elem *next;
 };
 
-/**
- * Find endpoint in the list ep_pair_list that is not already connected to addr.
- * If all endpoints are already connected to addr, return NULL.
- */
 nccl_net_ofi_ep_t *nccl_ofi_get_ep_for_addr(ep_pair_list_elem_t *ep_pair_list, void *addr)
 {
-	//print_addr("get_ep_for_addr", addr);
 	ep_pair_list_elem_t *ep_pair;
 
 	DL_FOREACH(ep_pair_list, ep_pair) {
 		hashed_addr_t *found_handle;
 		HASH_FIND(hh, ep_pair->pair.addr_set, (char *)addr, MAX_EP_ADDR, found_handle);
 		if (found_handle) {
+			/* This ep already has a connection to the address, skip to next*/
 			continue;
 		} else {
 			/* We found an ep that is not connected to addr, so return it */
@@ -58,12 +63,8 @@ nccl_net_ofi_ep_t *nccl_ofi_get_ep_for_addr(ep_pair_list_elem_t *ep_pair_list, v
 	return NULL;
 }
 
-/**
- * Add ep to the list ep_pair_list, with a single connection to addr.
- */
 void nccl_ofi_insert_ep_for_addr(ep_pair_list_elem_t *ep_pair_list, nccl_net_ofi_ep_t *ep, void *addr) {
 
-	//print_addr("nccl_ofi_insert_ep_for_addr", addr);
 	hashed_addr_t *new_addr = malloc(sizeof(*new_addr));
 	if (!new_addr) abort();
 	memcpy(new_addr->addr, addr, MAX_EP_ADDR);
@@ -77,15 +78,13 @@ void nccl_ofi_insert_ep_for_addr(ep_pair_list_elem_t *ep_pair_list, nccl_net_ofi
 	DL_APPEND(ep_pair_list, new_pair);
 }
 
-/**
- * Remove ep from the list ep_pair_list
- */
 void nccl_ofi_delete_ep_for_addr(ep_pair_list_elem_t *ep_pair_list, nccl_net_ofi_ep_t *ep)
 {
 	ep_pair_list_elem_t *ep_pair, *ep_pair_tmp;
 	DL_FOREACH_SAFE(ep_pair_list, ep_pair, ep_pair_tmp) {
 		if (ep_pair->pair.ep == ep) {
 			hashed_addr_t *e, *tmp;
+			/* Delete all addr entries in this ep's hashset */
 			HASH_ITER(hh, ep_pair->pair.addr_set, e, tmp) {
 				HASH_DEL(ep_pair->pair.addr_set, e);
 				free(e);
@@ -95,5 +94,4 @@ void nccl_ofi_delete_ep_for_addr(ep_pair_list_elem_t *ep_pair_list, nccl_net_ofi
 			return;
 		}
 	}
-	abort();
 }

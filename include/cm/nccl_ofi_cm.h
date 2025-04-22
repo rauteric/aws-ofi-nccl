581fa8c9ac8a468c784c154cc1ed067e54ac0871
Eric Raut <eraut@amazon.com>
2025-04-22 15:38:39 -0700

    Update to a more sensible public interface
    Makes the public interface (nccl_ofi_cm.h) leaner, with clear
    distinction of which methods (public) should be used by the caller
    (transport) and which methods are private implementation details.
    
    Moved some functionality that will become separate internal objects for
    better separation of concerns.
    
    Implementation is not done yet, so this definitely does not compile.

--- include/cm/nccl_ofi_cm.h
+++ include/cm/nccl_ofi_cm.h
@@ -9,62 +9,230 @@
 
 #include <deque>
 #include <memory>
-#include <optional>
-#include <unordered_map>
 #include <vector>
 
 #include "nccl_ofi.h"
-#include "nccl_ofi_freelist.h"
 #include "nccl_ofi_idpool.h"
 
 #include "cm/nccl_ofi_cm_types.h"
 #include "cm/nccl_ofi_cm_comms.h"
 
+
 /**
- * Simple helper FunctionObject to finalize a freelist, making it usable
- * with unique_ptr.
+ * Callback for transport to populate the connect response message
+ *
+ * @param transport_connect_msg:
+ * 	connect message data provided by the sender
+ *
+ * @param opaque_input:
+ * 	provides transport-chosen state to this function. This will be equal to the pointer
+ * 	passed to listener() constructor.
+ *
+ * @param opaque_output:
+ *      state returned by this function that will be available in the
+ *      receiver_info object once the connection is established
+ *
+ *      For example, this might be a pointer to the transport endpoint created
+ *      by this function.
  */
-class freelist_deleter
+typedef void (*transport_select_conn_resp_fn)(const void *transport_connect_msg,
+					      void *opaque_input,
+					      void *transport_connect_resp_msg,
+					      void **opaque_output);
+
+/**
+ * A listener returned by connection_manager::listen(), used to accept incoming
+ * connections from listen() and create a receiver info object
+ */
+class nccl_ofi_cm_listener
 {
 public:
-	void operator()(nccl_ofi_freelist_t *freelist)
+	/**
+	 * Obtain the handle associated with this listener
+	 *
+	 * The caller (transport) should return this handle back to NCCL to
+	 * be delivered out-of-band to the remote (send side) node
+	 */
+	nccl_net_ofi_conn_handle get_handle() { return handle; }
+
+	/**
+	 * Accept an incoming connection from the send side to this listener
+	 * This returns a receiver_info object that can be used to create the
+	 * protocol-specific communicator.
+	 *
+	 * If the connection is not yet established, returns empty.
+	 *
+	 * See documentation below on nccl_ofi_cm_receiver_info
+	 */
+	std::optional<nccl_ofi_cm_receiver_info> accept();
+
+	/* Destructor, frees associated resources */
+	~nccl_ofi_cm_listener();
+
+private:
+	/**
+	 * Construct a new cm listener object.
+	 *
+	 * (Note: transport should use connection_manager::connect() instead
+	 *  of constructing this object directly)
+	 *
+	 * @param cm:
+	 *      An instance of the connect manager for this domain
+	 *
+	 * @param transport_select_conn_resp_callback:
+	 *      Transport callback to create conn response message
+	 *
+	 * @param transport_select_conn_resp_input:
+	 * 	Transport-selected state to be provided to the callback
+	 */
+	nccl_ofi_cm_listener(nccl_ofi_connection_manager &cm,
+		transport_select_conn_resp_fn transport_select_conn_resp_callback,
+		void *transport_select_conn_resp_input);
+
+	nccl_ofi_connection_manager &cm;
+	uint32_t listener_id;
+	nccl_net_ofi_conn_handle handle;
+	std::deque<nccl_ofi_cm_receiver_info> receiver_info_queue;
+
+	transport_select_conn_resp_fn transport_select_conn_resp_callback;
+	void *transport_select_conn_resp_input;
+
+	friend class nccl_ofi_connection_manager;
+};
+
+
+/**
+ * Represents a completed connection. Provides information for the receiver side
+ * of a completed connection, intended to be used by the transport to create a
+ * recv communicator. This is the object returned by cm_listener->accept() when
+ * a corresponding connection is established.
+ */
+class nccl_ofi_cm_receiver_info
+{
+public:
+	/**
+	 * Return opaque output pointer provided by transport from
+	 * transport_select_conn_resp_fn for this connection
+	 */
+	void *get_transport_select_output()
 	{
-		nccl_ofi_freelist_fini(freelist);
+		return transport_select_output;
 	}
+
+	/**
+	 * Return connect message data from sender.
+	 *
+	 * Note: Returns a pointer to memory owned by this object. The memory
+	 * is valid until this object is destroyed.
+	 */
+	const void *get_conn_msg()
+	{
+		return user_conn_msg.data();
+	}
+
+	/**
+	 * Return connect response message data from sender.
+	 *
+	 * Note: Returns a pointer to memory owned by this object. The memory
+	 * is valid until this object is destroyed.
+	 */
+	const void *get_conn_resp_msg()
+	{
+		return user_conn_resp_msg.data();
+	}
+
+private:
+
+	nccl_ofi_cm_receiver_info(std::vector<uint8_t> conn_msg, std::vector<uint8_t> conn_resp_msg,
+				  void *transport_select_output);
+
+
+	std::vector<uint8_t> user_conn_msg;
+	std::vector<uint8_t> user_conn_resp_msg;
+
+	void *transport_select_output;
 };
 
 /**
- * Select rails callback
- *
- * opaque_input: provides transport-chosen state to this function
- *
- * opaque_output: state returned by this function in addition to the rail
- *                information, can be retrieved from cm_r_comm by transport
- * 		  after connection is established.
- *
- * 		  For example, this might be a pointer to the transport
- * 		  endpoint created by this function.
- *
- * TODO: The C++ way to do this, and avoid void ptr casting, is to make this
- *       a template parameter. However, the way the CM code is currently
- *       organized, this means making every other CM class a template class
- *       as well, and generally makes the code messy. May revisit this in the
- *       future
+ * A connector returned by connection_manager::connect(), used to connect
+ * to a remote node (recv side) given a handle.
  */
-typedef nccl_ofi_cm_ep_rail_info (*select_rails_fn)(const nccl_ofi_cm_ep_rail_info &sender_rails,
-						    void *opaque_input, void **opaque_output);
+class nccl_ofi_cm_send_connector
+{
+public:
+	/**
+	 * Test whether the connection is complete. This will return ready=true
+	 * when the connect message has been delivered and the connect response
+	 * message has been received.
+	 *
+	 * @param ready: set to true when connection complete
+	 * @return: negative errno code on error (in sending the connect response
+	 * 	    message)
+	 */
+	int test_ready(bool *ready);
+
+	/**
+	 * Pointer to data returned from receiver side in the connect response
+	 * message, once connection is complete. Note: this data is invalidated
+	 * when send_connector is destroyed
+	 */
+	void *get_conn_resp_msg();
+
+	/* Destructor, to free associated resources */
+	~nccl_ofi_cm_send_connector();
+
+private:
+
+	/**
+	 * Construct a new send connector
+	 *
+	 * (Note: transport should use connection_manager::connect() instead)
+	 *
+	 * @param cm: associated connection manager
+	 * @param handle: handle from listener on a remote node
+	 * @param transport_connect_msg:
+	 * 	pointer to transport-provided connect message
+	 */
+	nccl_ofi_cm_send_connector(nccl_ofi_connection_manager *cm,
+		nccl_net_ofi_conn_handle handle,
+		const void *transport_connect_msg);
+
+	fi_addr_t dest_addr;
+
+	void set_conn_resp_msg(const nccl_ofi_cm_conn_msg &conn_resp_msg);
+
+	void set_conn_msg_delivered() {
+		conn_msg_delivered = true;
+	}
+
+	/* Back-pointer to connection manager */
+	nccl_ofi_connection_manager *cm;
+
+	std::vector<uint8_t> conn_msg;
+	std::vector<uint8_t> conn_resp_msg;
+	nccl_ofi_freelist_elem_t *send_elem;
+	nccl_ofi_cm_send_conn_req send_conn_req;
+	std::optional<nccl_ofi_cm_conn_msg> received_conn_resp_msg;
+
+	bool conn_msg_sent;
+	bool conn_msg_delivered;
+
+	uint32_t s_comm_id;
+
+	void prepare_conn_msg(nccl_net_ofi_conn_handle *handle, nccl_ofi_cm_conn_msg *conn_msg);
+
+	friend class nccl_ofi_connection_manager;
+};
 
 /**
- * Connection manager. Top-level class that the caller can call connection
- * establishment functions, listen(), connect, accept(), to establish
- * send/recv communicators (TODO not really communicators, rename).
+ * Connection manager. Represents state of the connection management code.
  *
- * Intent is for client code to store and initialize a connection manager
- * per (transport-specific) endpoint.
+ * Intent is for client code to store and initialize a connection manager per
+ * (transport-specific) domain.
  *
- * Maintains a separate fid_ep and state that will be shared across all comms
- * created using this connection manager instance. The created endpoint will
- * be bound to the caller-supplied completion queue.
+ * The CM code maintains a separate Libfabric endpoint and state that will be
+ * shared across all connections created using this connection manager instance.
+ * The created endpoint will be bound to the caller-supplied completion queue.
  */
 class nccl_ofi_connection_manager
 {
@@ -73,156 +241,77 @@ public:
 	 * Initializes the CM system state. Creates an endpoint and posts
 	 * initial buffer pool
 	 *
-	 * @param cq: the completion queue to bind the new endpoint to.
-	 *            Ops submitted through the CM code will have a context
-	 *            pointer to nccl_net_ofi_context_t, with appropriate
-	 *            completion handling functions
+	 * @param info, domain:
+	 *      Libfabric info and domain objects against which the CM endpoint
+	 *      will be created
+	 *
+	 * @param cq:
+	 *      the completion queue to bind the new endpoint to. Ops submitted
+	 *      through the CM code will have a context pointer to
+	 *      nccl_net_ofi_context_t, with appropriate completion handling
+	 *      functions
+	 *
+	 * @param mr_key_pool:
+	 *      caller's mr_key_pool associated with domain. This ensures CM's
+	 *      memory registrations unique MR keys that don't conflict with
+	 *      other parts of the code
+	 *
+	 * @param conn_msg_size:
+	 *      size of transport-specific part of connect and connect response
+	 *      messages
 	 */
-	nccl_ofi_connection_manager(fi_info *info, fid_domain *_domain,
-				    fid_cq *cq, size_t num_comm_ids,
-				    nccl_ofi_idpool_t *_mr_key_pool);
+	nccl_ofi_connection_manager(fi_info *info, fid_domain *domain, fid_cq *cq,
+				    nccl_ofi_idpool_t &mr_key_pool, size_t conn_msg_size);
 
 	/**
 	 * Destructor. Finalizes CM endpoint and other state.
 	 *
-	 * Note: the connection manager should not be destroyed until all
-	 * associated cm communicators (TODO: rename from communicators) are
-	 * destroyed
+	 * Note: when the connection manager is destroyed, all associated
+	 * listeners and connectors are invalidated.
 	 */
 	~nccl_ofi_connection_manager();
 
 	/**
-	 * Return a communicator (TODO rename to handle or some such) that can
-	 * be used to accept incoming connections from connect()
-	 */
-	nccl_ofi_cm_l_comm *listen();
-
-	/**
-	 * Given a handle (produced by cm_l_comm on a remote node), establish
-	 * a (cm_s_comm) connection to the remote node. This will return a
-	 * cm_s_comm handle that can be queried until the connection is fully
-	 * established.
+	 * Create a new listener to accept connections
 	 *
-	 * @param handle: handle from cm_l_comm on a remote node
-	 * @param rail_info: rail information (addresses of data-transfer endpoints to send
-	 * 		     to the remote node)
-	 */
-	nccl_ofi_cm_s_comm *connect(nccl_net_ofi_conn_handle *handle,
-				    const nccl_ofi_cm_ep_rail_info &rail_info);
-
-
-	/**
-	 * Provide a "rail selector" used to choose rails to advertise
-	 * in the connect response message.
-	 */
-	void set_rail_selector(select_rails_fn fn, void *fn_input)
-	{
-		rail_selector = fn;
-		rail_selector_input = fn_input;
-	}
-	/* --------------------------------------------------------- */
-
-	/* TODO: the functions below are not intended to be used by the caller
-	   (i.e., transport), but are public due to use by other classes in the
-	   cm code. Revisit the separation of concerns here. */
-
-	/**
-	 * Map from comm id to l_comm. Used for receiving connect message
-	 */
-	nccl_ofi_cm_l_comm *get_l_comm(uint32_t l_comm_id);
-	/**
-	 * Map from comm id to s_comm. Used for receiving connect response message
-	 */
-	nccl_ofi_cm_s_comm *get_s_comm(uint32_t s_comm_id);
-
-	/**
-	 * Access functions for the conn_msg freelist (which takes care of memory
-	 * registration)
-	 */
-	nccl_ofi_freelist_elem_t *alloc_conn_msg()
-	{
-		return nccl_ofi_freelist_entry_alloc(conn_msg_fl.get());
-	}
-	void free_conn_msg(nccl_ofi_freelist_elem_t *conn_msg)
-	{
-		nccl_ofi_freelist_entry_free(conn_msg_fl.get(), conn_msg);
-	}
-
-	/**
-	 * Accessors for OFI resources
-	 */
-	fid_ep *get_ep() {return ep;}
-	fid_domain *get_domain() {return domain;}
-
-	/**
-	 * Accessor for CM-managed address vector. Used by cm_s_comm and cm_l_comm
-	 * to insert address from received connect/connect-response messages
+	 * @param transport_select_conn_resp_callback:
+	 *      Caller (transport) - provided callback to provide the
+	 *      transport-specific connect response message. The transport may
+	 *      select information for the connect response message based on the
+	 *      connect message from sender (e.g., endpoint-per-comm mode)
 	 *
-	 * TODO just make an accessor for fid_av* instead?
+	 * 	See doc above on transport_select_conn_resp_fn for parameters
+	 *
+	 * @param transport_select_conn_resp_input:
+	 * 	Transport-selected state to be provided to the callback
 	 */
-	int av_insert_address(const ep_name address, fi_addr_t *fi_addr);
-
-	const cm_ep_name &get_conn_ep_name() {return conn_ep_name;}
-
-	nccl_ofi_idpool_t *get_l_comm_id_pool() { return &l_comm_id_pool; }
-	nccl_ofi_idpool_t *get_data_comm_id_pool() { return &data_comm_id_pool; }
-
-	std::unordered_map<uint32_t, nccl_ofi_cm_l_comm *> *get_l_comm_map()
-	{ return &l_comm_map; }
-
-	std::unordered_map<uint32_t, nccl_ofi_cm_s_comm *> *get_s_comm_map()
-	{ return &s_comm_map; }
-
-	nccl_ofi_idpool_t *get_mr_key_pool() {return mr_key_pool;}
-
-	void append_pending_req(nccl_ofi_cm_req *req)
-	{
-		pending_reqs.push_back(req);
-	}
+	std::unique_ptr<nccl_ofi_cm_listener> listen(
+		transport_select_conn_resp_fn transport_select_conn_resp_callback,
+		void *transport_select_conn_resp_input);
 
 	/**
-	 * Post any reqs pending due to EAGAIN.
+	 * Establish a new connection to the listener identified by handle
+	 *
+	 * Returns a connector object that can be used to query for completion
+	 * of the connection and obtain the response from the receiver/
+	 *
+	 * @param handle: handle from listener on a remote node
+	 * @param transport_connect_msg:
+	 * 	Connect message. This should point to a buffer of size
+	 * 	conn_msg_size (parameter to constructor)
 	 */
-	int process_pending_reqs();
-
-	nccl_ofi_cm_ep_rail_info select_rails(const nccl_ofi_cm_ep_rail_info &sender_rails,
-					      void **opaque_output)
-	{
-		return rail_selector(sender_rails, rail_selector_input, opaque_output);
-	}
+	std::unique_ptr<nccl_ofi_cm_send_connector> connect(
+		nccl_net_ofi_conn_handle handle,
+		const void *transport_connect_msg);
 private:
-	/* Input */
-	fid_domain *domain;
-	/* Created by CM */
-	fid_ep *ep;
-	fid_av *av;
 
-	std::unordered_map<uint32_t, nccl_ofi_cm_l_comm *> l_comm_map;
-	std::unordered_map<uint32_t, nccl_ofi_cm_s_comm *> s_comm_map;
+	/* TODO store cm endpoint here */
 
-	std::unique_ptr<nccl_ofi_freelist_t, freelist_deleter> conn_msg_fl;
+	nccl_ofi_idpool_t listener_id_pool;
+	nccl_ofi_idpool_t connector_id_pool;
 
-	/* This must appear after conn_msg_fl so it is destructed first,
-	   since rx_req destructor returns buffer to freelist */
-	std::vector<std::unique_ptr<nccl_ofi_cm_rx_req>> rx_req_list;
-
-	/* rx reqs that need to be posted (due to EAGAIN) */
-	std::deque<nccl_ofi_cm_req *> pending_reqs;
-
-	nccl_ofi_idpool_t l_comm_id_pool;
-	nccl_ofi_idpool_t data_comm_id_pool;
-
-	/* Input to constructor */
-	nccl_ofi_idpool_t *mr_key_pool;
-
-	cm_ep_name conn_ep_name;
-
-	select_rails_fn rail_selector = NULL;
-	void *rail_selector_input = NULL;
-
-	void set_conn_ep_name();
-
-	void init_rx_buffers();
+	size_t conn_msg_size;
 };
+
 
 #endif /* NCCL_OFI_CM_H_ */
